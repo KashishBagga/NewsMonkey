9be0b79e3c0135b98616adf44b648370c6d7b3c0
Kashish <baggakashish4@gmail.com>
2022-11-26 13:42:57 +0530

    Update to functional based components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,11 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-export class NavBar extends Component {
-
-  render() {
+const NavBar = () =>{
     return (
       <div>
-                    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+                    <nav className="navbar fixed-top navbar-dark navbar-expand-lg bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">NewsMonkey</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
@@ -34,7 +32,6 @@ export class NavBar extends Component {
             </nav>
       </div>
     )
-  }
 }
 
 export default NavBar
